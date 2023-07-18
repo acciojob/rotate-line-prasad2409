3fd57b580fd7e41499e0f3fb93b9021a22ed7ce9
Durga Prasad <97454573+prasad2409@users.noreply.github.com>
2023-07-18 12:23:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+const line = document.getElementById('line');
+
+    // Set the rotation angle
+    let angle = 0;
+
+    // Function to rotate the line
+    function rotateLine() {
+      angle += 1; 
+
+      line.style.transform = `rotate(${angle}deg)`;
+
+      setTimeout(rotateLine, 10);
+    }
+
+    rotateLine();
